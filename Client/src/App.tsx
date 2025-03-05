745ab59e8150dc02e8053d644f7eef472ee98e8b
Jimmykotter <jimmykotter@gmail.com>
2025-03-04 20:11:15 -0600

    added the quesion and answer sections for the quizpage

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import UserLogin from "./Components/Login";
 // import KnowledgeLevel from "./Components/KnowledgeLevel";
-// import Quiz from "./Components/Quiz";
+import Quiz from "./Components/Quiz";
 // import Results from "./Components/Results";
-import "./App.css";
+// import "./App.css";
 
 function App() {
 
@@ -13,7 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<UserLogin />} />
         {/* <Route path="/knowledge-level" element={<KnowledgeLevel />} /> */}
-        {/* <Route path="/quiz" element={<Quiz />} /> */}
+        <Route path="/quiz" element={<Quiz />} />
         {/* <Route path="/results" element={<Results />} /> */}
         
       </Routes>
